fea49faddc5415519aa9fe5cc9d8f14103596f1e
ad1s0n <zdrian.alewski@gmail.com>
2024-11-30 00:37:40 +0100

    detection as code with scapy

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,22 +1,25 @@
 from nfstream import NFStreamer
 from report import Report
+from scapy.all import *
 import time
 
 
 class Analyzer:
     def __init__(self, pcap_filename: str) -> None:
-        self.packet_stream = NFStreamer(source=pcap_filename)
+        self.network_flow_stream = NFStreamer(source=pcap_filename)
+        self.scapy_packets = rdpcap(pcap_filename)
         self.report = Report()
     
-    def detect_large_traffic(self):
-        for flow in self.packet_stream:
-            if ((flow.dst_port == 443 or
-                flow.dst_port == 80) and 
-                flow.src2dst_bytes > 1000000):
-                self.report.add_alert("WARNING", "Large Traffic Detected", flow.bidirectional_first_seen_ms, {"flow": "data"})
-
+    def detect_suspicious_domains(self, blacklist):
+        for packet in self.scapy_packets:
+            if packet.haslayer(DNS) and packet.qr == 0:
+                domain = packet.qd.qname.decode("utf-8")[:-1]
+                if domain in blacklist:
+                    self.report.add_alert("MALICIOUS", "Malicious domain detected", int(packet.time), {"domain": domain})
+    
+    
     def get_flow_statistics(self):
-        for flow in self.packet_stream:
+        for flow in self.network_flow_stream:
             self.report.add_flow_statistic(
                 flow.src_ip, flow.dst_ip, flow.src_port, flow.dst_port, flow.protocol, 
                 flow.src2dst_bytes, flow.dst2src_bytes, flow.bidirectional_bytes, 
@@ -27,7 +30,7 @@ class Analyzer:
 
 if __name__ == "__main__":
     analyzer = Analyzer("test.pcap")
-    analyzer.detect_large_traffic()
     analyzer.get_flow_statistics()
+    analyzer.detect_suspicious_domains(["madmrx.duckdns.org"])
     print(analyzer.report.to_json())
         
